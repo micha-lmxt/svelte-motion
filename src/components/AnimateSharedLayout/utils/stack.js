12d54b9c58903d131905978a029c9a18221043e1
Michael Lucht <github@lucht-familie.de>
2021-04-23 21:54:48 +0200

    Update stack.js

--- src/components/AnimateSharedLayout/utils/stack.js
+++ src/components/AnimateSharedLayout/utils/stack.js
@@ -1,3 +1,8 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
+
 import { __assign } from 'tslib';
 import { Presence } from '../types.js';
 import { elementDragControls } from '../../../gestures/drag/VisualElementDragControls.js';
