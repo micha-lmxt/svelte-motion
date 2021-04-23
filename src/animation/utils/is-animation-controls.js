8ca6893e39947322ef57f339eaf9a1ebed9b5f90
Michael Lucht <github@lucht-familie.de>
2021-04-23 12:40:16 +0200

    Update is-animation-controls.js

--- src/animation/utils/is-animation-controls.js
+++ src/animation/utils/is-animation-controls.js
@@ -1,3 +1,8 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
+
 var isAnimationControls = function (v) {
     return typeof v === "object" && typeof (v).start === "function"
 };
