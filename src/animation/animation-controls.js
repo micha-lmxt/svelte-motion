2a95fc19da05132a62d66c4012aaa85d3ae4bf1f
Michael Lucht <github@lucht-familie.de>
2021-04-23 12:43:34 +0200

    Update animation-controls.js

--- src/animation/animation-controls.js
+++ src/animation/animation-controls.js
@@ -1,3 +1,8 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
+
 import { __spreadArray, __read } from 'tslib';
 //import { invariant } from 'hey-listen';
 import { stopAnimation, animateVisualElement } from '../render/utils/animation.js';
