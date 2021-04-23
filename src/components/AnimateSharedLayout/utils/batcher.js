c85545692fe93201bcd97ed93c070b8aa1685b59
Michael Lucht <github@lucht-familie.de>
2021-04-23 21:53:22 +0200

    Update batcher.js

--- src/components/AnimateSharedLayout/utils/batcher.js
+++ src/components/AnimateSharedLayout/utils/batcher.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 
 import { getFrameData, flushSync } from "framesync"
 import { compareByDepth } from '../../../render/utils/compare-by-depth.js';
