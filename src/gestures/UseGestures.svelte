1a66521e4255b7484451f576f7d7beea2d7153c5
Michael Lucht <github@lucht-familie.de>
2021-04-24 09:26:48 +0200

    Update UseGestures.svelte

--- src/gestures/UseGestures.svelte
+++ src/gestures/UseGestures.svelte
@@ -1,4 +1,9 @@
 <script>
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
+
 import { UsePanGesture } from "./use-pan-gesture";
 import { UseTapGesture } from "./use-tap-gesture"
 import { UseHoverGesture } from "./use-hover-gesture"
