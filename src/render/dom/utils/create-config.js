405cb8d51b7ce452aba8773a84949f706b0abf29
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:23:37 +0200

    Update create-config.js

--- src/render/dom/utils/create-config.js
+++ src/render/dom/utils/create-config.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import { __assign } from 'tslib';
 import { svgMotionConfig } from '../../svg/config-motion.js';
 import { htmlMotionConfig } from '../../html/config-motion.js';
