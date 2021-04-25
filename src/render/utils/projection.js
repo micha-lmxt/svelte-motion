d620b6961f183ed5681af890703bbe8c3156d747
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:41:14 +0200

    Update projection.js

--- src/render/utils/projection.js
+++ src/render/utils/projection.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import { resetBox, applyTreeDeltas } from '../../utils/geometry/delta-apply.js';
 import { updateBoxDelta } from '../../utils/geometry/delta-calc.js';
 
