bd481018156ca860204b910cb75d0a8ea8d67cee
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:37:27 +0200

    Update render.js

--- src/render/svg/utils/render.js
+++ src/render/svg/utils/render.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import { camelToDash } from '../../dom/utils/camel-to-dash.js';
 import { renderHTML } from '../../html/utils/render.js';
 import { camelCaseAttributes } from './camel-case-attrs.js';
