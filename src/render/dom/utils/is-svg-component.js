fb36b626b04245fabb4d24e2fb4ff8e8b710091e
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:24:49 +0200

    Update is-svg-component.js

--- src/render/dom/utils/is-svg-component.js
+++ src/render/dom/utils/is-svg-component.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import { lowercaseSVGElements } from '../../svg/lowercase-elements.js';
 
 function isSVGComponent(Component) {
