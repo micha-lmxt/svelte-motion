eb046caeb177c2ec50509e0e123262e961943fff
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:28:34 +0200

    Update create-motion-class.js

--- src/render/dom/create-motion-class.js
+++ src/render/dom/create-motion-class.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import Motion from '../../motion/Motion.svelte';
 import { loadFeatures } from "../../motion/features/definitions"
 
