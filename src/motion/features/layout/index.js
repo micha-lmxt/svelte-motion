2b22bfbbfc0afef7c602be02dff18e61c8373bfc
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:14:19 +0200

    Update index.js

--- src/motion/features/layout/index.js
+++ src/motion/features/layout/index.js
@@ -1,3 +1,8 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
+
 import { AnimateLayoutContextProvider } from './Animate.js';
 import { MeasureContextProvider } from './Measure.js';
 
