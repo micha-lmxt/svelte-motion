74b4c66d25758b5ec5cf6d27ecc27b779a2e86d6
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:18:48 +0200

    Update UseVisualState.svelte

--- src/motion/utils/UseVisualState.svelte
+++ src/motion/utils/UseVisualState.svelte
@@ -1,4 +1,9 @@
 <script context="module">
+    /** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
+    
     import { resolveMotionValue } from "../../value/utils/resolve-motion-value";
     import {
         checkIfControllingVariants,
