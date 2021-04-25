e76411f2af8bb2aea9cf9fc7235d444e701f36c9
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:36:54 +0200

    Update create-render-state.js

--- src/render/svg/utils/create-render-state.js
+++ src/render/svg/utils/create-render-state.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import { __assign } from 'tslib';
 import { createHtmlRenderState } from '../../html/utils/create-render-state.js';
 
