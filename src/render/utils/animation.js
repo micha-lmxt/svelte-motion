5e2291b16e894549f7ac00ede6e1c1bff92c2965
Michael Lucht <github@lucht-familie.de>
2021-04-25 21:39:34 +0200

    Update animation.js

--- src/render/utils/animation.js
+++ src/render/utils/animation.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 import { __read, __rest, __assign } from 'tslib';
 import { startAnimation } from '../../animation/utils/transitions.js';
 import { setTarget } from './setters.js';
