432115a8f13f7089ef199cd15846cef1ec29a45b
Michael Lucht <github@lucht-familie.de>
2021-04-25 22:27:11 +0200

    Update is-ref-object.js

--- src/utils/is-ref-object.js
+++ src/utils/is-ref-object.js
@@ -1,3 +1,7 @@
+/** 
+based on framer-motion@4.0.3,
+Copyright (c) 2018 Framer B.V.
+*/
 function isRefObject(ref) {
     return (typeof ref === "object" &&
         Object.prototype.hasOwnProperty.call(ref, "current"));
