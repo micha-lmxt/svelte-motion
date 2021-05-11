c956ff23935dae6b3dc5c23a379075d50710c30b
micha-lmxt <michael@gradientdescent.de>
2021-05-11 16:48:48 +0200

    fix fix-process-env

--- src/utils/fix-process-env.js
+++ src/utils/fix-process-env.js
@@ -1,13 +1,22 @@
 
 const fix = ()=>{
+    try{
+        
+        if (!process.env){
+            process.env={};
+        }
+        return true;;
+    }catch(e){}
+    
     if (!window || (window.process && window.process.env)){
         return false;
     }
+    
     if (!window.process){
         window.process={}
     }
     window.process.env={};
-    return true;
+    return true;    
 }
 
 export const fixed = fix();
