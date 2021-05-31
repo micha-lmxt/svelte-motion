2c96517b4f9d50b1a75e0c1aba242ec855465337
micha-lmxt <michael@gradientdescent.de>
2021-05-31 17:33:34 +0200

    complete scroll motion values

--- src/value/scroll/use-element-scroll.js
+++ src/value/scroll/use-element-scroll.js
@@ -1,1 +1,68 @@
-export { default as UseElementScroll } from './UseElementScroll.svelte';
+/** 
+based on framer-motion@4.1.16,
+Copyright (c) 2018 Framer B.V.
+*/
+import {
+    createScrollMotionValues,
+    createScrollUpdater,
+} from "./utils"
+import { addDomEvent } from "../../events/use-dom-event"
+import { onDestroy, tick } from "svelte"
+
+const getElementScrollOffsets = (element) => () => {
+    return {
+        xOffset: element.scrollLeft,
+        yOffset: element.scrollTop,
+        xMaxOffset: element.scrollWidth - element.offsetWidth,
+        yMaxOffset: element.scrollHeight - element.offsetHeight,
+    }
+}
+
+export const useElementScroll = (ref) => {
+    
+    const values = createScrollMotionValues();
+    let cleanup;
+    const setScroll = ()=>{
+        cleanup?.();
+
+        if ( (!ref || !ref.current) && !values.ref ){
+            return;
+        }
+        const element = (ref && ref.current) ? ref : values.ref;
+
+        const updateScrollValues = createScrollUpdater(
+            values,
+            getElementScrollOffsets(element)
+        )
+
+        const scrollListener = addDomEvent(
+            element,
+            "scroll",
+            updateScrollValues,
+            { passive: true }
+        )
+
+        const resizeListener = addDomEvent(
+            element,
+            "resize",
+            updateScrollValues
+        )
+        cleanup = ()=>{
+            scrollListener && scrollListener()
+            resizeListener && resizeListener()
+        }
+    }
+
+
+    if (ref && ref.current){
+        setScroll()
+    }else{
+        tick().then(setScroll)
+    }
+
+    onDestroy(()=>cleanup?.())
+    
+    return values;
+}
+
+//export { default as UseElementScroll } from './UseElementScroll.svelte';
--- src/value/scroll/use-viewport-scroll.js
+++ src/value/scroll/use-viewport-scroll.js
@@ -1,1 +1,90 @@
-export { default as UseViewportScroll } from './UseViewportScroll.svelte';
+/** 
+based on framer-motion@4.1.16,
+Copyright (c) 2018 Framer B.V.
+*/
+import {
+    createScrollMotionValues,
+    createScrollUpdater,
+} from "./utils"
+import { addDomEvent } from "../../events/use-dom-event"
+import { tick } from "svelte"
+
+let viewportScrollValues
+
+
+function getViewportScrollOffsets() {
+    return {
+        xOffset: window.pageXOffset,
+        yOffset: window.pageYOffset,
+        xMaxOffset: document.body.clientWidth - window.innerWidth,
+        yMaxOffset: document.body.clientHeight - window.innerHeight,
+    }
+}
+
+let hasListeners = false
+
+function addEventListeners() {
+    hasListeners = true
+    if (typeof window === "undefined") return
+
+    const updateScrollValues = createScrollUpdater(
+        viewportScrollValues,
+        getViewportScrollOffsets
+    )
+
+    addDomEvent(window, "scroll", updateScrollValues, { passive: true })
+    addDomEvent(window, "resize", updateScrollValues)
+}
+
+/**
+ * Returns MotionValues that update when the viewport scrolls:
+ *
+ * - `scrollX` — Horizontal scroll distance in pixels.
+ * - `scrollY` — Vertical scroll distance in pixels.
+ * - `scrollXProgress` — Horizontal scroll progress between `0` and `1`.
+ * - `scrollYProgress` — Vertical scroll progress between `0` and `1`.
+ *
+ * **Warning:** Setting `body` or `html` to `height: 100%` or similar will break the `Progress`
+ * values as this breaks the browser's capability to accurately measure the page length.
+ *
+ * @library
+ *
+ * ```jsx
+ * import * as React from "react"
+ * import {
+ *   Frame,
+ *   useViewportScroll,
+ *   useTransform
+ * } from "framer"
+ *
+ * export function MyComponent() {
+ *   const { scrollYProgress } = useViewportScroll()
+ *   return <Frame scaleX={scrollYProgress} />
+ * }
+ * ```
+ *
+ * @motion
+ *
+ * ```jsx
+ * export const MyComponent = () => {
+ *   const { scrollYProgress } = useViewportScroll()
+ *   return <motion.div style={{ scaleX: scrollYProgress }} />
+ * }
+ * ```
+ *
+ * @public
+ */
+export function useViewportScroll() {
+    /**
+     * Lazy-initialise the viewport scroll values
+     */
+    if (!viewportScrollValues) {
+        viewportScrollValues = createScrollMotionValues()
+    }
+
+    tick().then(_=>{
+        !hasListeners && addEventListeners()
+    })
+    
+    return viewportScrollValues
+}
