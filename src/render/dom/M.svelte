fa8eb2964b9b2e1aac1f737dfa8c79bc6af0b751
Michael Lucht <micha-lmxt@gradientdescent.de>
2022-11-15 10:30:53 +0100

    set type to module

--- src/render/dom/M.svelte
+++ src/render/dom/M.svelte
@@ -1,14 +1,11 @@
 <script>
-    import T from './Transformer.svelte';
     import Motion from "../../motion/Motion.svelte";
-
     export let ___tag;
-
 </script>
 
 <Motion {...$$restProps} let:props let:motion>
-    <T {motion} ___tag={___tag} {...props}>
+    <svelte:element this={___tag} use:motion {...props}>
         <slot />
-    </T>
+    </svelte:element>
 </Motion>
 
